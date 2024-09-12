0d6ad70801ee6e61dc79b37985eb4e9fee6b5a67
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-12 17:59:51 -0300

    Navbar atualizada

--- edit-task.php
+++ edit-task.php
@@ -91,14 +91,20 @@
     </style>
 </head>
 <body>
-    <header class="navbar navbar-expand-sm bg-dark d-flex align-items-center justify-content-between p-3">
-        <h2>Olá, <?= $_SESSION["name"]; ?></h2>
-
-        <abbr title="Sair">
-            <a href="logout.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="40px" fill="#fff"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h292.66v66.67H186.67v586.66h292.66V-120H186.67Zm470.66-176.67-47-48 102-102H360v-66.66h351l-102-102 47-48 184 184-182.67 182.66Z"/></svg>
-            </a>
-        </abbr>
+    <header class="navbar navbar-expand-sm bg-dark navbar-dark d-flex align-items-center justify-content-between p-3 mb-5">
+        <h2 class="">Olá, <?= $_SESSION["name"]; ?></h2>
+        
+        <ul class="navbar-nav d-flex align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add-task.php">Nova tarefa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Sair</a>
+                </li>
+        </ul>
     </header>
 
     <div class="container">
--- home.php
+++ home.php
@@ -57,14 +57,20 @@
     </style>
 </head>
 <body>
-    <header class="navbar navbar-expand-sm bg-dark d-flex align-items-center justify-content-between p-3 mb-5">
-        <h2>Olá, <?= $_SESSION["name"]; ?></h2>
-
-        <abbr title="Sair">
-            <a href="logout.php">
-                <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="40px" fill="#fff"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h292.66v66.67H186.67v586.66h292.66V-120H186.67Zm470.66-176.67-47-48 102-102H360v-66.66h351l-102-102 47-48 184 184-182.67 182.66Z"/></svg>
-            </a>
-        </abbr>
+    <header class="navbar navbar-expand-sm bg-dark navbar-dark d-flex align-items-center justify-content-between p-3 mb-5">
+        <h2 class="">Olá, <?= $_SESSION["name"]; ?></h2>
+        
+        <ul class="navbar-nav d-flex align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add-task.php">Nova tarefa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Sair</a>
+                </li>
+        </ul>
     </header>
 
     <main class="container">
@@ -82,7 +88,7 @@
                         // Adiciona a classe 'done' se a tarefa estiver concluída
                         $task_class = $task["status"] == "completed" ? "done" : "";
 
-                        echo "<div class='shadow-sm border rounded container-fluid d-flex align-items-center justify-content-between p-2 mb-4 $task_class'>";
+                        echo "<div class='shadow-sm border rounded container-fluid d-flex align-items-center justify-content-between p-2 mb-3 $task_class'>";
                         echo "<h5>" . $task["title"] . "</h6>";
                         echo "<div class='btn-container'>
                                 <a href='delete-task.php?task_id=" . $task["id"] . "' class='btn btn-danger p-2'>
@@ -101,7 +107,7 @@
                         echo "</div>";
                     }
                 } else {
-                    echo "<p>Nenhuma tarefa encontrada.</p>";
+                    echo "<p class='text-center'>Nenhuma tarefa encontrada.</p>";
                 }
             ?>
         </div>
