6fe3629ce0cbb9921ef8123a20ed1c2ee395aac1
Fellipe Castro <fellipecastro27d@gmail.com>
2024-09-12 21:52:21 -0300

    Projeto atualizado

--- add-task.php
+++ add-task.php
@@ -76,21 +76,21 @@
 <body>
     <header class="navbar navbar-expand-sm bg-dark navbar-dark d-flex align-items-center justify-content-between p-3 mb-5">
         <h2 class="">Olá, <?= $_SESSION["name"]; ?></h2>
-        
-        <ul class="navbar-nav d-flex align-items-center">
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add-task.php">Nova tarefa</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Sair</a>
-                </li>
-        </ul>
+
+        <abbr title="Sair">
+            <a href="logout.php">
+                <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="40px" fill="#fff"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-586.66q0-27 19.83-46.84Q159.67-840 186.67-840h292.66v66.67H186.67v586.66h292.66V-120H186.67Zm470.66-176.67-47-48 102-102H360v-66.66h351l-102-102 47-48 184 184-182.67 182.66Z"/></svg>
+            </a>
+        </abbr>
     </header>
 
     <div class="container">
+        <abbr title="Volar">
+            <a href="home.php">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 448 512"><path fill="#212529" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+            </a>
+        </abbr>
+
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post" autocomplete="off">
             <h2 class="mb-3 text-center">Nova tarefa</h2>
 
